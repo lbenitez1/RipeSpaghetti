7fcde01be1e0fbe8046504a25b5dba4c67a38f24
lbenitez1 <leonardo.daniel.benitez@gmail.com>
2016-04-24 22:09:29 -0400

    4/24/2016

--- app/src/main/java/com/leonardobenitez/leonardobenitez/ripespaghetti/UserActivity.java
+++ app/src/main/java/com/leonardobenitez/leonardobenitez/ripespaghetti/UserActivity.java
@@ -14,20 +14,48 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        // display hello to user
         final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMessage);
-
         Intent intent = getIntent();
         String name = intent.getStringExtra("username");
         String cell = intent.getStringExtra("cell");
-
+        // construct welcome message to user
         String message = "Hello "+name;
         welcomeMessage.setText(message);
 
     }
-    public void uploadTopicButton(View v){
-        //do something
-        Intent upload = new Intent(getApplicationContext(),UserActivity.class);
-        UserActivity.this.startActivity(upload);
+    public void userInfoButtonOnClick(View v){
+        // view user info and edit
+        Intent userInfo = new Intent(getApplicationContext(),UserActivity.class);
+        UserActivity.this.startActivity(userInfo);
     }
+    public void signOutButtonOnClick(View v){
+        // sign out
+        Intent signOut = new Intent(getApplicationContext(),LoginActivity.class);
+        UserActivity.this.startActivity(signOut);
+    }
+    public void aboutButtonOnClick(View v){
+        // sign out
+        Intent about = new Intent(getApplicationContext(),LoginActivity.class);
+        UserActivity.this.startActivity(about);
+    }
+    public void newsButtonOnClick(View v){
+        // view news in music
+        Intent news = new Intent(getApplicationContext(),UserActivity.class);
+        UserActivity.this.startActivity(news);
+    }
+    public void albumDirectoryButtonOnClick(View v){
+        // view all albums in directory
+        Intent directory = new Intent(getApplicationContext(),UserActivity.class);
+        UserActivity.this.startActivity(directory);
+    }
+    public void helpButtonOnClick(View v){
+        // get help
+        Intent help = new Intent(getApplicationContext(),UserActivity.class);
+        UserActivity.this.startActivity(help);
+    }
+    public void searchAlbumOnClick(View v){
+        // search album
 
+    }
 }
